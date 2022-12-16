67aca3dcce015684e0aa9abc00e2809ffb16e7d1
Raheel Bennett <raheel.bennett@telus.com>
2022-12-15 20:29:51 -0700

    refactor getAllReservations function in database.js to link with our database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,7 +83,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, title, cost_per_night, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, title, cost_per_night
+  ORDER BY start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then(result => result.rows)
+    .catch(err => err.message);
+
 }
 exports.getAllReservations = getAllReservations;
 
